50dee0b04d9f5c03e11b10c5b8e7fde3276c8bf2
so8os <ahmadothman033@gmail.com>
2025-01-20 14:19:43 +0400

    test

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@
 
 import Features from "../components/Features";
 import CTASection from "../components/CTASection";
-import Hero from "../components/Hero";
 import HowItWorks from "@/components/How";
 import Logos from "@/components/logos";
 import Testimonials from "@/components/Testimonials";
@@ -12,9 +11,13 @@ import RequestQuote from "@/components/RequestQuote";
 export default function LandingPage() {
   return (
     <main className="w-full py-12 md:py-24 lg:py-32 xl:py-48">
-      <Hero />
+      {/* <Hero /> */}
       <iframe
-        src="http://localhost:3000/"
+        src={
+          process.env.NODE_ENV === "development"
+            ? "http://localhost:3000/"
+            : "https://drone-scene-chi.vercel.app/"
+        }
         className="w-full h-[500px] bg-transparent"
       ></iframe>
       <Features />
